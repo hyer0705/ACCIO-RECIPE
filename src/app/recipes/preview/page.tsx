d3a950f0db24cdd84f52e7e65c279188999c63b4
조혜린 <hyer1205@gmail.com>
2026-03-01 21:21:40 +0900

    fix: 안쓰는 코드 제거

--- src/app/recipes/preview/page.tsx
+++ src/app/recipes/preview/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Header from '@/components/layout/Header';
 import RecipePreview from '@/components/recipe/RecipePreview';
